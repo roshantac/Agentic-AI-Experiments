e0e4a9c9f7e07d1f157e92cae21c503a759a0e92
Muhammed Roshan <roshantac@gmail.com>
2025-10-26 07:43:25 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyAtjvXqC9Lb87fvKlytD0NIEkGLnzefBng")
+genai.configure(api_key="add_ur_key")
 client = genai.GenerativeModel("gemini-2.0-flash")
 
 def llm_generate(prompt):
